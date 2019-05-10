ffdf8118805e010e1e180be63b6342bc7e6d13c7
PMB <85dstudios@gmail.com>
2019-05-09 20:56:08 -0400

    Organization and first hardware class
    Patrick Bradley
    5-9-2019

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OffSeason2019/Configuration/RobotCfg2019.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OffSeason2019/Configuration/RobotCfg2019.java
@@ -1,8 +1,15 @@
 package org.firstinspires.ftc.teamcode.OffSeason2019.Configuration;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+
 public class RobotCfg2019 {
 
-    
+
 
 
 
